ccf3e4c4c5b8b4384e940f0c304a35b67caa464a
Abrilskop <uberpot.ia@gmail.com>
2024-11-17 23:41:27 -0500

    chore: add superpoderes

--- SuperHeroePOO/Program.cs
+++ SuperHeroePOO/Program.cs
@@ -3,6 +3,23 @@
 var superman = new SuperHeroe();
 var batman = new SuperHeroe();
 
+// Superpoderes
+var superVolar = new SuperPoder();
+superVolar.Nombre = "Volar";
+superVolar.Descripcion = "Capacidad de Volar";
+superVolar.Nivel = (int)NivelPoder.N1;
+
+
+var SuperVelocidad = new SuperPoder();
+SuperVelocidad.Nombre = "Velocidad";
+SuperVelocidad.Descripcion = "Poder romper la cara de sonido";
+SuperVelocidad.Nivel = (int)NivelPoder.N2;
+
+var SuperFuerza = new SuperPoder();
+SuperFuerza.Nombre = "Super Fuerza";
+SuperFuerza.Descripcion = "Habilidad para romper de todo";
+SuperFuerza.Nivel = (int)NivelPoder.N2;
+
 superman.Id = 1;
 superman.Nombre = "Superman";
 superman.IdentidadSecreta = "Clark Kent";
@@ -10,6 +27,7 @@ superman.Ciudad = "Metropolis";
 superman.PuedeVolar = true;
 superman.SuperPoderes = new[] { "Vision de rayos X", "Volar", "Super fueza", "Ojos rayos lazer" };
 
+// class
 class SuperHeroe
 {
     public int Id;
@@ -18,4 +36,15 @@ class SuperHeroe
     public string Ciudad;
     public string[] SuperPoderes;
     public bool PuedeVolar;
+}
+class SuperPoder
+{
+    public string Nombre;
+    public string Descripcion;
+    public int Nivel; 
+}
+// al asignar 1 nivel siempre usar esos valores
+enum NivelPoder
+{
+    N1, N2, N3
 }
