1f877e3c23b97855c0233428b91f43f26706d840
Abrilskop <uberpot.ia@gmail.com>
2024-11-17 23:48:57 -0500

    chore: asignarle superpoderes a la lista de poderes de superman

--- SuperHeroePOO/Program.cs
+++ SuperHeroePOO/Program.cs
@@ -1,14 +1,10 @@
 ﻿
-// creacion de objeto superman a partir de la clase superheroe
-var superman = new SuperHeroe();
-var batman = new SuperHeroe();
 
 // Superpoderes
 var superVolar = new SuperPoder();
 superVolar.Nombre = "Volar";
 superVolar.Descripcion = "Capacidad de Volar";
 superVolar.Nivel = (int)NivelPoder.N1;
-
 
 var SuperVelocidad = new SuperPoder();
 SuperVelocidad.Nombre = "Velocidad";
@@ -20,12 +16,22 @@ SuperFuerza.Nombre = "Super Fuerza";
 SuperFuerza.Descripcion = "Habilidad para romper de todo";
 SuperFuerza.Nivel = (int)NivelPoder.N2;
 
+// Superheroes
+var superman = new SuperHeroe();
 superman.Id = 1;
 superman.Nombre = "Superman";
 superman.IdentidadSecreta = "Clark Kent";
 superman.Ciudad = "Metropolis";
 superman.PuedeVolar = true;
-superman.SuperPoderes = new[] { "Vision de rayos X", "Volar", "Super fueza", "Ojos rayos lazer" };
+List<SuperPoder> poderesSuperman = new List<SuperPoder>(); // creacion del objeto de tipo lista(coleccion de elementos)
+poderesSuperman.Add(SuperFuerza);
+poderesSuperman.Add(superVolar);
+poderesSuperman.Add(SuperVelocidad);
+
+superman.SuperPoderes = poderesSuperman;// asignamos lista
+
+var batman = new SuperHeroe();
+
 
 // class
 class SuperHeroe
@@ -34,7 +40,7 @@ class SuperHeroe
     public string Nombre;
     public string IdentidadSecreta;
     public string Ciudad;
-    public string[] SuperPoderes;
+    public List<SuperPoder> SuperPoderes; // clase dentro de otra clase
     public bool PuedeVolar;
 }
 class SuperPoder
