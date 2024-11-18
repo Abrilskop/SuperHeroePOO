01a798351da46ef0141eb547d183cfd19dcb0148
Abrilskop <uberpot.ia@gmail.com>
2024-11-17 23:12:47 -0500

    feat: add class superheroe

---
+++ SuperHeroePOO/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// creacion de objeto superman a partir de la clase superheroe
+var superman = new SuperHeroe();
+var batman = new SuperHeroe();
+
+superman.Id = 1;
+superman.Nombre = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+superman.SuperPoderes = new[] { "Vision de rayos X", "Volar", "Super fueza", "Ojos rayos lazer" };
+
+class SuperHeroe
+{
+    public int Id;
+    public string Nombre;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public string[] SuperPoderes;
+    public bool PuedeVolar;
+}
